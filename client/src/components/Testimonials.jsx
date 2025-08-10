348fbbcd8c9c08ea0543928fcd740d0448ddcb8a
Aadi Designer <aadityasuryawanshi44@gmail.com>
2025-08-11 00:16:51 +0530

    Home, Steps, Intro And Testimonials are Done

---
+++ client/src/components/Testimonials.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { testimonialsData, assets } from '../assets/assets'          // Assuming testimonialsData is an array of testimonial objects with name, image, and text from assets.js
+
+function Testimonials() {
+  return (
+    <div className='flex flex-col items-center justify-center my-20 py-12'> 
+    <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>
+        Customer Testimonials
+    </h1>
+    <p className='text-gray-500 mb-12'>
+        What our users are saying about us
+    </p>
+
+    <div className='flex flex-wrap gap-6'>
+        {testimonialsData.map((testimonial, index)=>(
+            <div key={index} className='bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all'>
+                <div className='flex flex-col items-center'>
+                    <img src={testimonial.image} alt="" className='rounded-full w-14'/>
+                    <h2 className='text-xl font-semibold mt-3'>{testimonial.name}</h2>
+                    <p className='text-gray-500 mb-4'>{testimonial.role}</p>
+                    <div className='flex mb-4'>
+                        {Array(testimonial.stars).fill().map((item,index)=>(
+                            <img key={index} src={assets.rating_star} alt=''/>
+                        ))}
+                    </div>
+                    <p className='text-center text-sm text-gray-600 '>{testimonial.text}</p>
+                </div>
+            </div>
+        ))}
+    </div>
+
+    </div>   // Copied the Classname from Description.jsx and modified the content
+  )
+}
+
+export default Testimonials
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import Steps from '../components/Steps.jsx'
+import Description from '../components/Description.jsx'
+import Testimonials from '../components/Testimonials.jsx'
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Steps/>
+      <Description/>
+      <Testimonials/>
+    </div>
+  )
+}
+
+export default Home
+
+// Taking from StepsData in assets.js, the data displayed in Home page is the Steps component which is imported from Steps.jsx
+// The Header component is also imported and displayed at the top of the Home page
